9a74e49f164f890d30e204129eb57760d404ff84
semik-serma <semikserma@gmail.com>
2025-12-16 20:01:26 +0545

    for rames sir

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
 'use client'
-import Link from 'next/navigation'
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 import Image from 'next/image'
+import Link from 'next/link'
+import { FaLinkedin } from "react-icons/fa";
 
-
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Navbar = () => {
+  const [date,setdate]=useState('')
+  
+
+    
+    
+
   return (
     <div>
         <div>
-            <div></div>
+            <div>
+              <Image src='https://images.unsplash.com/photo-1620288627223-53302f4e8c74?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'width={40} height={40} alt='Tiger gang'></Image>
+            </div>
             <div>
         
       
@@ -19,6 +29,17 @@ const Navbar = () => {
       
     
             </div>
+            <div>
+              <Link href='/login'>Login</Link>
+            </div>
+            <div>
+            <Link href='https://www.facebook.com/'><FaFacebook /></Link>
+            <Link href='https://www.youtube.com/@digitalmediacenterphidim6022'> <FaYoutube  /></Link>
+            <Link href='https://www.linkedin.com/feed/'><FaLinkedin /></Link>
+            </div>
+          <div>
+            
+          </div>
         </div>
       <div></div>
     </div>
